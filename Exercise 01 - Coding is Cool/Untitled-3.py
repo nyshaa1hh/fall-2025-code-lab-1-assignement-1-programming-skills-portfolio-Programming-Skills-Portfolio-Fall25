db4fc80c276f4d5ebcac71da26f0f77ad62a8e6f
nyshaa1hh <neishanahan74@gmail.com>
2025-10-29 11:27:33 +0400

    HEllo

---
+++ Exercise 01 - Coding is Cool/Untitled-3.py
@@ -0,0 +1,15 @@
+def display_message(message):
+    print(message)
+
+def favourite_book(title):
+
+
+    print(title+" is my favourite book!")
+
+favourite_book("13 reasons why")
+
+def shirt_size(size):
+    print("The shirt size is "+size)
+
+    
+shirt_size('Large')
