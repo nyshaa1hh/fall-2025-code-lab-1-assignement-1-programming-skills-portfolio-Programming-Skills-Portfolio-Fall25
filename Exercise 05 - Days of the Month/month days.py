2eeec498970a07765dcfc4d9493ad263e2f4b1fa
nyshaa1hh <neishanahan74@gmail.com>
2025-11-21 17:49:31 +0400

    Update month days.py

--- Exercise 05 - Days of the Month/month days.py
+++ Exercise 05 - Days of the Month/month days.py
@@ -1,12 +1,12 @@
-month_num = int(input("Enter Month (1-12): "))
-if month_num == 2:
-    if input("Leap year? (yes/no): ") == 'yes':
+num_month = int(input("Enter Month (1-12): "))
+if num_month == 2:
+    if input("is it Leap year? (yes/no): ") == 'yes':
         print("29 days")
     else:
         print("28 days")
-elif month_num in [1, 3, 5, 7, 8, 10, 12]:
+elif num_month in [1, 3, 5, 7, 8, 10, 12]:
     print("31 days")
-elif month_num in [4, 6, 9, 11]:
+elif num_month in [4, 6, 9, 11]:
     print("30 days")
 else:
     print("Invalid month")
