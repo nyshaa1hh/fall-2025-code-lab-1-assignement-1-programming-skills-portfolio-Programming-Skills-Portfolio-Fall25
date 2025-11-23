73735c4d92f28defa24ff6e3e78c21ad1aadeca5
nyshaa1hh <neishanahan74@gmail.com>
2025-11-23 22:00:12 +0400

    Update biography.py

--- Exercise 03 - Biography/biography.py
+++ Exercise 03 - Biography/biography.py
@@ -10,6 +10,6 @@ def biography():
     return {"name": name, "hometown": hometown, "age": age}
 
 def bio():
-    user_information = biography()
-    print(f"Name: {user_information['name']}\nHometown: {user_information['hometown']}\nAge: {user_information['age']}")                                                       
+    bio = biography()
+    print(f"Name: {bio['name']}\nCountry: {bio['hometown']}\nAge: {bio['age']}")                                                       
 bio()
