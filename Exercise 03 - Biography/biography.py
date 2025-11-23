64783052b84bbf1e36f1a595897443796d2f303d
nyshaa1hh <neishanahan74@gmail.com>
2025-11-23 22:22:54 +0400

    Update biography.py

--- Exercise 03 - Biography/biography.py
+++ Exercise 03 - Biography/biography.py
@@ -2,14 +2,14 @@ def biography():
     name = input("Enter your full name: ")
     hometown = input("Enter your home country: ")
     age = input("Enter your age: ")
-
+    sex= input("Enter your gender: ")
     if age.isdigit():
         age = int(age)
     else:
         age = "age is invalid!"
-    return {"name": name, "hometown": hometown, "age": age}
+    return {"name": name, "country": hometown, "age": age, "gender": sex}
 
 def bio():
     bio = biography()
-    print(f"Name: {bio['name']}\nCountry: {bio['hometown']}\nAge: {bio['age']}")                                                       
+    print(f"Name: {bio['name']}\nCountry: {bio['country']}\nAge: {bio['age']}\nGender: {bio['gender']}")                                                       
 bio()
